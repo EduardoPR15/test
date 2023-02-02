c974d1525e76bea151e4d9e164815bf02b3300a8
EduardoPR15 <lpeduardo15@gmail.com>
2023-02-02 08:31:20 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System.Net;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: UserController
+        public ActionResult Index()
+        {
+            //variable con credenciales
+            var token = HttpContext.Session.GetString("_UserToken");
+
+            if (token != null)
+            {
+
+                return RedirectToAction("Bienvenida");
+            }
+            else
+                return View();
+
+
+
+            //return View();
+        }
+
+
+
+        // POST: UserController/Create
+        [HttpPost]
+        //[HttpPost]
+        public async Task<IActionResult> Index(UserModel userModel)
+        {
+
+
+            string error = "Credenciales incorrectas";
+            var client = new RestClient("https://crazy-stonebraker.68-168-208-58.plesk.page/api/Auth/Auth");
+            var request = new RestSharp.RestRequest();
+            request.Method = Method.Post;
+            request.AddParameter("nombre", userModel.nombre);
+            request.AddParameter("password", userModel.password);
+
+            request.AlwaysMultipartFormData = true;
+            RestResponse response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                HttpContext.Session.SetString("_UserToken", "ok");
+                return RedirectToAction("index");
+            }
+            else
+            {
+                Console.WriteLine(response.Content);
+                return View();
+            }
+
+        }
+        public ActionResult Bienvenida()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace test.Models
+{
+    public class UserModel
+    {
+        public string nombre { get; set; }
+        public string password { get; set; }
+    }
+}
